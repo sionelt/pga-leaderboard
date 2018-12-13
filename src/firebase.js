3a71631f2dde0665e7159b31fcef4c9314d38628
sionelt <sltafengatoto@gmail.com>
2018-12-13 07:40:51 -0700

    secure firebase config secrets in env

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 // Initialize Firebase
 const config = {
-  apiKey: 'AIzaSyA9voxAWmd38g5QtZHE9RvZAKcoCo8SXTA',
-  authDomain: 'pga-leaderboard.firebaseapp.com',
-  databaseURL: 'https://pga-leaderboard.firebaseio.com',
-  projectId: 'pga-leaderboard',
-  storageBucket: 'pga-leaderboard.appspot.com',
-  messagingSenderId: '895785262497',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 }
 const fire = firebase.initializeApp(config)
 
