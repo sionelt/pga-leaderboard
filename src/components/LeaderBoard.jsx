f98aff7af9702102d745012c48528287578b49fa
sionelt <sltafengatoto@gmail.com>
2018-12-13 03:25:05 -0700

    update leaderbox with events handlers

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,29 +1,34 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from '@material-ui/core/styles'
-import Table from '@material-ui/core/Table'
+import AddPersonIcon from '@material-ui/icons/PersonAdd'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import AddPersonIcon from '@material-ui/icons/PersonAdd'
+import Table from '@material-ui/core/Table'
+import Paper from '@material-ui/core/Paper'
 
 const styles = theme => ({
   row: {
     cursor: 'pointer',
     '&:hover': {
       background: theme.palette.grey[50],
-      boxShadow: ''
-    }
-  }
+      boxShadow: '',
+    },
+  },
+  tableWrapper: {
+    overflow: 'auto',
+    // height: '100%',
+    // minHeight: '100%'
+  },
 })
 
-class LeaderBoard extends Component {
-  render() {
-    const {players, classes} = this.props
-    return (
+const LeaderBoard = props => {
+  const {players, classes, addNewPlayer, updatePlayer, removePlayer} = props
+  return (
+    <Paper className={classes.tableWrapper}>
       <Table padding="checkbox">
         <TableHead>
           <TableRow>
@@ -31,10 +36,10 @@ class LeaderBoard extends Component {
             <TableCell numeric>Score</TableCell>
             <TableCell numeric>
               <Button
-                variant="contained"
-                color="primary"
                 size="small"
-                className={classes.fab}
+                color="primary"
+                variant="contained"
+                onClick={addNewPlayer}
               >
                 New
                 <AddPersonIcon size="small" style={{marginLeft: '5px'}} />
@@ -43,21 +48,42 @@ class LeaderBoard extends Component {
           </TableRow>
         </TableHead>
         <TableBody>
-          {players.map(p => (
-            <TableRow key={p.id} className={classes.row} hover={true}>
-              <TableCell>{`${p.lastName}, ${p.firstName}`}</TableCell>
-              <TableCell numeric>{p.score}</TableCell>
-              <TableCell numeric>
-                <Button variant="outlined" color="secondary" size="small">
-                  Delete
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
+          {players.length > 0
+            ? players.map(p => (
+                <TableRow
+                  key={p.id}
+                  hover={true}
+                  selected={p.selected}
+                  className={classes.row}
+                  onClick={updatePlayer(p.id)}
+                >
+                  <TableCell>{`${p.lastName}, ${p.firstName}`}</TableCell>
+                  <TableCell numeric>{p.score}</TableCell>
+                  <TableCell numeric>
+                    <Button
+                      size="small"
+                      color="secondary"
+                      variant="outlined"
+                      onClick={removePlayer(p.id)}
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            : null}
         </TableBody>
       </Table>
-    )
-  }
+    </Paper>
+  )
+}
+
+LeaderBoard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  players: PropTypes.arrayOf(PropTypes.object).isRequired,
+  addNewPlayer: PropTypes.func.isRequired,
+  updatePlayer: PropTypes.func.isRequired,
+  removePlayer: PropTypes.func.isRequired,
 }
 
 export default withStyles(styles)(LeaderBoard)
